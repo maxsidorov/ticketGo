f0ba6f386fcf9c6c3bf199564607964604127862
maxsidorov <sidorovmaksimgithub@gmail.com>
2025-06-09 16:14:34 +0300

    Update event_storage.go

--- storage/event_storage.go
+++ storage/event_storage.go
@@ -59,16 +59,3 @@ func (s *EventStorage) Create(event *Event) (int, error) {
     }
     return events, nil
   }
-// Модель данных
-type Event struct {
-  ID        int
-  Title     string
-  Date      time.Time
-  Place  string
-  Decsription string
-  Price     float64
-  Tickets   int
-  Sold_tickets int
-  Image     string
-  Discount  float64
-}
