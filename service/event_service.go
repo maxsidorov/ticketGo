747223e062a26dc87afc7a20c067f644271bbcbc
maxsidorov <sidorovmaksimgithub@gmail.com>
2025-06-09 16:16:28 +0300

    Update event_service.go

---
+++ service/event_service.go
@@ -0,0 +1,44 @@
+package service
+
+import (
+	"models"//
+	"storage"//
+	"errors"
+	"time"
+)
+
+type EventService struct {
+	storage *storage.EventStorage
+}
+
+func NewEventService(storage *storage.EventStorage) *EventService {
+	return &EventService{storage: storage}
+}
+
+func (s *EventService) CreateEvent(event *models.Event) (int, error) {
+	if event.Date.Before(time.Now()) {
+		return 0, errors.New("event date must be in the future")
+	}
+	return s.storage.Create(event)
+}
+
+func (s *EventService) GetEvents(page, pageSize int) ([]models.Event, error) {
+	if page < 1 { page = 1 }
+	if pageSize < 1 || pageSize > 100 { pageSize = 10 }
+	return s.storage.GetAll(page, pageSize)
+}
+
+func (s *EventService) GetEvent(id int) (*models.Event, error) {
+	return s.storage.GetByID(id)
+}
+
+func (s *EventService) UpdateEvent(event *models.Event) error {
+	if event.Date.Before(time.Now()) {
+		return errors.New("cannot update past events")
+	}
+	return s.storage.Update(event)
+}
+
+func (s *EventService) DeleteEvent(id int) error {
+	return s.storage.Delete(id)
+}
