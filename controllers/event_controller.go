25c9691cd85626601b1592d275dafa6a11036ce8
xar <mihail.sholov@yandex.ru>
2025-06-11 16:43:53 +0300

    sort full

--- controllers/event_controller.go
+++ controllers/event_controller.go
@@ -3,26 +3,46 @@ package controllers
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/maxsidorov/ticketGo/models"
+	"log"
 	"net/http"
 	"sort"
 )
-
-// var DB *gorm.DB // должен быть инициализирован в main.go
 
 func ShowMainPage(c *gin.Context) {
 	q := c.Query("q")
 	var events []models.Event
 	query := DB
-
 	if q != "" {
 		query = query.Where("title ILIKE ?", "%"+q+"%")
 	}
 	query.Find(&events)
-	p := c.Query("but")
-	if p != "" {
-		sort.Slice(events, func(i, j int) bool { return events[i].Title < events[j].Title })
-	}
 	// TODO: username из сессии
+	c.HTML(http.StatusOK, "index.html", gin.H{
+		"events":   events,
+		"username": c.GetString("username"),
+	})
+}
+
+func MainPage(c *gin.Context) {
+	var events []models.Event
+	query := DB
+	query.Find(&events)
+	p := c.PostForm("but")
+	r := c.PostForm("phone")
+	if p != "" {
+		switch r {
+		case "SortName":
+			sort.Slice(events, func(i, j int) bool { return events[i].Title < events[j].Title })
+		case "SortPrice":
+			sort.Slice(events, func(i, j int) bool { return events[i].Price < events[j].Price })
+		case "SortDate":
+			sort.Slice(events, func(i, j int) bool { return events[i].DateTime.Before(events[j].DateTime) })
+		case "По категории":
+			log.Printf("Category!!!!!!!!!!!")
+		}
+	} else {
+		log.Printf("but")
+	}
 	c.HTML(http.StatusOK, "index.html", gin.H{
 		"events":   events,
 		"username": c.GetString("username"),
