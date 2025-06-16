2bbab4f5e941242e0db998d9f5e21e17ef7fea64
XardingEvil <mihail.sholov@yandex.ru>
2025-06-16 12:34:49 +0300

    filter
    added filter

--- controllers/event_controller.go
+++ controllers/event_controller.go
@@ -5,6 +5,7 @@ import (
 	"github.com/maxsidorov/ticketGo/models"
 	"net/http"
 	"sort"
+	"time"
 )
 
 func MainPage(c *gin.Context) {
@@ -12,12 +13,28 @@ func MainPage(c *gin.Context) {
 	search := c.Query("search")
 	but_sort := c.Query("but_sort")
 	type_sort := c.Query("sort")
+	date_first := c.Query("date_first")
+	date_last := c.Query("date_second")
 
 	var events []models.Event
 
 	query := DB
 	if but_search != "" || search != "" {
 		query = query.Where("title ILIKE ?", "%"+search+"%")
+	}
+	if date_first != "" {
+		startDate, _ := time.Parse("2006-01-02", date_first)
+		startDate = time.Date(startDate.Year(), startDate.Month(), startDate.Day(),
+			0, 0, 0, 0,
+			time.Local)
+		query = query.Where("date_time >= ?", startDate)
+	}
+	if date_last != "" {
+		endDate, _ := time.Parse("2006-01-02", date_last)
+		endDate = time.Date(endDate.Year(), endDate.Month(), endDate.Day()+1,
+			0, 0, 0, 0,
+			time.Local)
+		query = query.Where("date_time < ?", endDate)
 	}
 	query.Find(&events)
 	if but_sort != "" {
@@ -33,8 +50,10 @@ func MainPage(c *gin.Context) {
 		}
 	}
 	c.HTML(http.StatusOK, "index.html", gin.H{
-		"events":   events,
-		"search":   search,
-		"username": c.GetString("username"),
+		"events":     events,
+		"search":     search,
+		"date_first": date_first,
+		"date_last":  date_last,
+		"username":   c.GetString("username"),
 	})
 }
